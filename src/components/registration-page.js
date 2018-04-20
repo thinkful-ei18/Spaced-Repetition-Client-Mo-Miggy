f46bbfa5d808273d4c8edf8dede43466932d5013
emptyelephants <emptyelephants@gmail.com>
2018-04-20 09:44:20 -0700

    fixed redirect issue

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -7,6 +7,9 @@ import RegistrationForm from './registration-form';
 export function RegistrationPage(props) {
   // If we are logged in (which happens automatically when registration
   // is successful) redirect to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
   return (
     <div className="home">
       <RegistrationForm />
